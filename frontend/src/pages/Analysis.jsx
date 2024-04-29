9ef41a58e2cf60656902b257a8e29485c47a34d6
tanisha0605 <tanishakanal@gmail.com>
2024-04-30 00:56:17 +0530

    Changed fetch route

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -9,7 +9,7 @@ function Analysis() {
 
   const fetchImages = async () => {
     try {
-      const response = await fetch('http://localhost:5000/');
+      const response = await fetch('http://127.0.0.1:5000/');
       if (!response.ok) {
         throw new Error(`Failed to fetch images: ${response.status} ${response.statusText}`);
       }
